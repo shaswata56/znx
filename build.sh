3a649df56dab7dffff0c627dfb04ff6ed1a7ce6a
luis-lavaire <llavaire01@gmail.com>
2019-09-20 06:31:54 -0600

    Test this build.

--- build.sh
+++ build.sh
@@ -11,11 +11,8 @@ TRAVIS_BRANCH=$2
 apt-get -qq -y update > /dev/null
 apt-get -qq -y install wget patchelf file libcairo2 > /dev/null
 apt-get -qq -y install mtools xorriso axel gdisk zsync btrfs-progs dosfstools grub-common grub2-common grub-efi-amd64 grub-efi-amd64-bin > /dev/null
-
 apt-get -qq -y install git autoconf gettext automake libtool-bin autopoint pkg-config libncurses5-dev bison > /dev/null
 
-wget -q https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool
-wget -q https://gitlab.com/nitrux/tools/build-utilities/raw/master/copier
 wget -q https://gitlab.com/nitrux/tools/build-utilities/raw/master/mkiso
 
 wget -q http://mirrors.kernel.org/ubuntu/pool/main/u/util-linux/libmount1_2.33.1-0.1ubuntu2_amd64.deb
@@ -23,8 +20,6 @@ wget -q http://mirrors.kernel.org/ubuntu/pool/main/u/util-linux/libsmartcols1_2.
 dpkg -i libmount1_2.33.1-0.1ubuntu2_amd64.deb
 dpkg -i libsmartcols1_2.33.1-0.1ubuntu2_amd64.deb
 
-chmod +x appimagetool
-chmod +x copier
 chmod +x mkiso
 chmod +x appdir/znx
 
@@ -51,56 +46,57 @@ rm /lib/x86_64-linux-gnu/libmount.so.1*
 sed -i "s/@TRAVIS_COMMIT@/${TRAVIS_COMMIT:0:7}/" appdir/znx
 
 
-# -- Copy binaries and its dependencies to appdir.
 
-./copier mkiso appdir
-./copier axel appdir
-./copier mcopy appdir
-./copier zsync appdir
-./copier lsblk appdir
-./copier sgdisk appdir
-./copier wipefs appdir
-./copier xorriso appdir
-./copier mkfs.vfat appdir
-./copier mkfs.btrfs appdir
+export ARCH="$(arch)"
+export VERSION="v0.0.1"
+
+APP=cmake-tools
+LOWERAPP=${APP,,}
+
+mkdir -p "$HOME/$APP/$APP.AppDir/usr/"
+
+BUILD_PATH="$(pwd)"
+
+cd "$HOME/$APP/"
+
+wget -q https://github.com/probonopd/AppImages/raw/master/functions.sh -O ./functions.sh
+. ./functions.sh
+
+cd $APP.AppDir
+
+cp -r "${BUILD_PATH}/cmake" .
+cp "${BUILD_PATH}/.clang-format" .
+cp "${BUILD_PATH}/.clang-tidy" .
+cp "${BUILD_PATH}/${LOWERAPP}" "AppRun"
+
+########################################################################
+# Copy desktop and icon file to AppDir for AppRun to pick them up
+########################################################################
+
+cp "${BUILD_PATH}/appimage/${LOWERAPP}.desktop" .
+cp "${BUILD_PATH}/appimage/${LOWERAPP}.png" .
+
+########################################################################
+# Copy in the dependencies that cannot be assumed to be available
+# on all target systems
+########################################################################
+
+copy_deps
 
 
-# -- Build GRUB's boot image.
+########################################################################
+# Patch away absolute paths; it would be nice if they were relative
+########################################################################
 
-grub-mkimage \
-	-C xz \
-	-O x86_64-efi \
-	-o appdir/bootx64.efi \
-	-p /boot/grub \
-	boot linux search normal configfile \
-	part_gpt btrfs ext2 fat iso9660 loopback \
-	test keystatus gfxmenu regexp probe \
-	efi_gop efi_uga all_video gfxterm font \
-	echo read ls cat png jpeg halt reboot
+find . -type f -exec sed -i -e 's|/usr|././|g' {} \;
+find . -type f -exec sed -i -e 's@././/bin/env@/usr/bin/env@g' {} \;
 
+########################################################################
+# AppDir complete
+# Now packaging it as an AppImage
+########################################################################
 
-# -- Generate the AppImage.
+cd .. # Go out of AppImage
 
-(
-	cd appdir
-
-	wget -q https://raw.githubusercontent.com/AppImage/AppImages/master/functions.sh
-	chmod +x functions.sh
-	. ./functions.sh
-	delete_blacklisted
-	rm functions.sh
-
-	find lib/x86_64-linux-gnu -type f -exec patchelf --set-rpath '$ORIGIN/././' {} \;
-	find bin -type f -exec patchelf --set-rpath '$ORIGIN/../lib/x86_64-linux-gnu' {} \;
-	find sbin -type f -exec patchelf --set-rpath '$ORIGIN/../lib/x86_64-linux-gnu' {} \;
-	find usr/bin -type f -exec patchelf --set-rpath '$ORIGIN/../../lib/x86_64-linux-gnu' {} \;
-	find usr/sbin -type f -exec patchelf --set-rpath '$ORIGIN/../../lib/x86_64-linux-gnu' {} \;
-)
-
-wget -q https://raw.githubusercontent.com/Nitrux/appimage-wrapper/master/appimage-wrapper
-chmod a+x appimage-wrapper
-
-UPDATE_URL="zsync|https://github.com/Nitrux/znx/releases/download/continuous-development/znx_$TRAVIS_BRANCH"
-
-mkdir out
-ARCH=x84_64 ./appimage-wrapper appimagetool -u "$UPDATE_URL" appdir out/znx_$TRAVIS_BRANCH
+mkdir -p ../out/
+generate_type2_appimage
